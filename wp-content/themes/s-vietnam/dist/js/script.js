afb7c72ac10aca5a5bb6fbab7607309b149012e5
Hai Cao <hai.cao@ffwagency.com>
2018-09-04 17:56:58 +0700

    Update

--- wp-content/themes/s-vietnam/dist/js/script.js
+++ wp-content/themes/s-vietnam/dist/js/script.js
@@ -4,6 +4,23 @@
 (function($) {
   $(document).ready(function() {
     // Call to function
+    $('.image-select-multiple').on('change', function() {
+      for (var i = 0; i < this.files.length; i++) {
+        var fr = new FileReader();
+        fr.onload = function(e) {
+          //$('#thumbs').append('<img src="' + e.target.result + '" width="50px" height="50px">')
+          //console.log(e.target.result);
+          $('ul.media-sortable').append('<li class="media-item" style="background-image: url(' + e.target.result + ');"><span class="media-delete-icon fa fa-times-circle-o"></span></li>');
+
+          $('.media-delete-icon').on('click', function() {
+            //alert('okokok');
+            $(this).parent().remove();
+          });
+        }
+        fr.readAsDataURL(this.files[i]);
+      }
+    });
+
     $( ".media-sortable" ).sortable();
     $( ".media-sortable" ).disableSelection();
   });
